6770504f952ff387b88f3b1a06a5475256121459
Ivan Petrov <ebkrc5bsfekd@mail.ru>
2025-07-01 15:00:03 +0300

    попытка подработать large layout

--- src/base-container/components/default-layout/LargeLayout.jsx
+++ src/base-container/components/default-layout/LargeLayout.jsx
@@ -5,7 +5,7 @@ import { useIntl } from '@edx/frontend-platform/i18n';
 import { Hyperlink, Image } from '@openedx/paragon';
 import classNames from 'classnames';
 
-import backgroundVideo from './background.gif'
+import backgroundImage from './background.gif';
 
 import messages from './messages';
 
@@ -13,15 +13,18 @@ const LargeLayout = () => {
   const { formatMessage } = useIntl();
 
   return (
-    <div className="w-50 d-flex bg-primary-500">
-      
-      <div className="col-md-12 bg-primary-100">
-        <div className="min-vh-100 d-flex align-items-center">
-          <div className={classNames({ 'large-yellow-line mr-n4.5': getConfig().SITE_NAME === 'edX' })} />
-          
-        </div>
-      </div>
-      
+    <div
+      className="d-flex justify-content-center align-items-center text-white text-center"
+      style={{
+        backgroundColor: '#FF0000',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <h1 style={{ fontSize: '3rem', fontWeight: 'bold', textShadow: '2px 2px 4px rgba(0,0,0,0.5)' }}>
+        Страница авторизации
+      </h1>
     </div>
   );
 };
